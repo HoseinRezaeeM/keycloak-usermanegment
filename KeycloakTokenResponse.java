9b801487e6f54714399df668dfca50c07b210ac5
Hossein Rezaei <143436672+HoseinRezaeeM@users.noreply.github.com>
2024-08-31 10:41:16 +0330

    Create KeycloakTokenResponse.java

---
+++ KeycloakTokenResponse.java
@@ -0,0 +1,28 @@
+package com.uaa.client.keycloak.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class KeycloakTokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+    @JsonProperty("refresh_expires_in")
+    private Integer refreshExpiresIn;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("token_type")
+    private String tokenType;
+    @JsonProperty("not-before-policy")
+    private Integer notBeforePolicy;
+    @JsonProperty("session_state")
+    private String sessionState;
+    @JsonProperty("scope")
+    private String scope;
+}
